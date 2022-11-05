4ffd1164826fcc77b16edd64f2af68eb2b8e58c7
Darius Tanz <scratchticketseb@gmail.com>
2022-11-05 04:31:53 +0000

    Nov 5, 2022, 12:31 AM

--- Smart Contracts/DeRollCore.sol
+++ Smart Contracts/DeRollCore.sol
@@ -26,7 +26,8 @@ contract DeRoll{
         uint256 NewPropriID = ProprietorContracts[msg.sender].length + 1;
         uint256 NewEmployAwID = EmployeeAwaiting[Employee].length + 1;
 
-        WorkContract NewWorkContract = WorkContract(msg.sender, pAlias, Employee, WorkDescription, false, false, msg.)
+        WorkContract NewWorkContract = WorkContract(msg.sender, pAlias, Employee, WorkDescription, false, false, msg.value);
+        
 
 
 
