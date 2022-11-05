8862ea228c75a5e89a0df48d93ef539bd3ea8a04
Darius Tanz <scratchticketseb@gmail.com>
2022-11-05 18:57:12 +0000

    Nov 5, 2022, 2:57 PM

--- Website/js/functionallity.js
+++ Website/js/functionallity.js
@@ -3,10 +3,10 @@
 
 async function DisplayEmployeePage(){
     document.getElementById('Proprietor').style.display = "none";
-    document.getElementById('Employee').style.display = "block";
+    document.getElementById('Employee').style.display = "inline-flex";
 }
 
 async function DisplayProprietorPage(){
     document.getElementById('Employee').style.display = "none";
-    document.getElementById('Proprietor').style.display = "block";
+    document.getElementById('Proprietor').style.display = "inline-flex";
 }
