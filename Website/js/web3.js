18653481665109e7e86c0d327cb4ab2ae51e775a
Darius Tanz <scratchticketseb@gmail.com>
2022-11-05 12:49:56 +0000

    Nov 5, 2022, 8:49 AM

--- Website/js/web3.js
+++ Website/js/web3.js
@@ -8,13 +8,6 @@ async function loginWithEth(){
     if(window.ethereum){
         await ethereum.request({ method: 'eth_requestAccounts' });
         window.web3 = await new Web3(ethereum);
-        await getID();
-        if (netID != 61){
-            console.log("The current Metamask/Web3 network is not Ethereum Classic, please connect to the Ropsten test network."); //CHANGE FOR REAL CROWDSALE TO ETC
-            alert("The current Metamask/Web3 network is not ETC, please connect to the Ethereum Classic test network.");
-            showOverlay();
-            return("Failed to connect")
-        }
         accountarray = await web3.eth.getAccounts();
         contract = await new window.web3.eth.Contract(ABI, contractAddress, window.web3);
         account = accountarray[0];
