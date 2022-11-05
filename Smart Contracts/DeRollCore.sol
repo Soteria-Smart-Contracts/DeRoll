f5366c9c252916b570f17d904c7c646018133ad4
Darius Tanz <scratchticketseb@gmail.com>
2022-11-05 03:49:49 +0000

    Nov 4, 2022, 11:49 PM

--- Smart Contracts/DeRollCore.sol
+++ Smart Contracts/DeRollCore.sol
@@ -24,7 +24,7 @@ contract DeRoll{
     mapping(address => mapping(uint256 => WorkContract)) EmployeeActive;
 
 
-    function CreateNewWorkContract(string pAlias, )
+    function CreateNewWorkContract(string pAlias, string address Employee, )
 
 
 
