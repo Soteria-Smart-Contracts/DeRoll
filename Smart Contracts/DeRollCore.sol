07016021bb3160e632d939909c524a67956459d8
Darius Tanz <scratchticketseb@gmail.com>
2022-11-05 03:37:18 +0000

    Nov 4, 2022, 11:37 PM

--- Smart Contracts/DeRollCore.sol
+++ Smart Contracts/DeRollCore.sol
@@ -18,7 +18,7 @@ contract DeRoll{
 
 
     mapping(address => mapping(uint256 => WorkContract)) ProprietorContracts;
-    mapping(address => )
+    mapping(address => mapping(uint256 => WorkContract)) 
 
 
 
