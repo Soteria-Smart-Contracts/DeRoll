4316bcdd978bc28b4105a78afb2c816231dd53d1
Darius Tanz <scratchticketseb@gmail.com>
2022-11-05 04:01:42 +0000

    Nov 5, 2022, 12:01 AM

--- Smart Contracts/DeRollCore.sol
+++ Smart Contracts/DeRollCore.sol
@@ -27,6 +27,7 @@ contract DeRoll{
     function CreateNewWorkContract(string memory pAlias, string memory eAlias, address Employee, string memory WorkDescription) public payable {
         require(msg.value >= 1000000000000000);
         
+        NewID
 
 
     }
