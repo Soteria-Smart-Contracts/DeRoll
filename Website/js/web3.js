e8f5a1388545a7e1cd6ffb527d1f786f12e64945
Darius Tanz <scratchticketseb@gmail.com>
2022-11-05 13:38:49 +0000

    Nov 5, 2022, 9:38 AM

--- Website/js/web3.js
+++ Website/js/web3.js
@@ -11,6 +11,7 @@ async function loginWithEth(){
         accountarray = await web3.eth.getAccounts();
         contract = await new window.web3.eth.Contract(ABI, contractAddress, window.web3);
         account = accountarray[0];
+        
     } else { 
         alert("No ETHER Wallet available")
     }
