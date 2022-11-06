64bfb7423ee791badc075611b8e821824e314d83
Darius Tanz <scratchticketseb@gmail.com>
2022-11-06 12:57:37 +0000

    Nov 6, 2022, 7:57 AM

--- Website/js/web3.js
+++ Website/js/web3.js
@@ -42,5 +42,5 @@ async function CreateWorkContract(){
         return("Invalid Address")
     }
 
-    await contract.methods.CreateNewWorkContract(pAlias, eAlias, Addy, Desc)
+    await contract.methods.CreateNewWorkContract(pAlias, eAlias, Addy, Desc).send({})
 }
