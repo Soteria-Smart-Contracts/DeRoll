f899df8b1e64334e532f166e9ecdd1032d451c68
Darius Tanz <scratchticketseb@gmail.com>
2022-11-06 17:41:46 +0000

    Nov 6, 2022, 12:41 PM

--- Website/js/web3.js
+++ Website/js/web3.js
@@ -68,6 +68,29 @@ async function UpdateProprietorContracts(){
     console.log(Contracts);
 }
 
+async function UpdateProprietorContracts(){
+    await loginWithEth();
+    PropIDs = await GetProprietorIDs();
+    let Contracts = [];
+    document.getElementById("pContracts").innerHTML = "";
+
+    index = 0;
+    while(index < PropIDs.length){
+        current = await GetContractDetails(PropIDs[index])
+        Contracts.push(current);
+        newcard = document.createElement("a");
+        newcard.className = "contractdetail";
+        newcard.id = index;
+        ether = Number(current[10]) / 1000000000000000000;
+        console.log(ether)
+        newcard.innerHTML = '<div id="pContracts" class="ContractBox"> <a class="contractdetail">Employee Alias: <e>' + current[2].toString() + '</e></a><a class="contractdetail">Proprietor Alias: <e>' + current[4].toString() + '</e></a><a class="contractdetail">Work Description: <e>' + current[5].toString() + '</e></a><a class="contractdetail">Ether to be paid: <e>' + ether + '</e></a></div>'
+        document.getElementById("pContracts").appendChild(newcard);
+        index++
+    }
+
+    console.log(Contracts);
+}
+
 async function GetEmployeeUnacceptedIDs(){
     index = 0;
     let PIDs = [];
