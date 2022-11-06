82621e5620875032e78728fd5609d9c500fbffec
Darius Tanz <scratchticketseb@gmail.com>
2022-11-06 12:40:17 +0000

    Nov 6, 2022, 7:40 AM

--- Website/js/web3.js
+++ Website/js/web3.js
@@ -34,6 +34,7 @@ async function CreateWorkContract(){
     EtherInput = BigInt(document.getElementById('EtherInput').value * 1000000000);
 
     Ether = BigInt(EtherInput * BigInt(1000000000)).toString();
+    console.log()
 
     if(Addy.length =! 42){
         alert("Employee Address is invalid")
