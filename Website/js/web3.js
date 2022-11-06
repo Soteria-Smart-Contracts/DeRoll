5197057cd439848e9c69f15246c20ad66c9ee53d
Darius Tanz <scratchticketseb@gmail.com>
2022-11-06 16:04:08 +0000

    Nov 6, 2022, 11:04 AM

--- Website/js/web3.js
+++ Website/js/web3.js
@@ -57,5 +57,6 @@ async function GetProprietorIDs(){
 }
 
 async function GetContractDetails(){
-    await contract.methods.WorkContracts(3).call()
+    Contract await contract.methods.WorkContracts(3).call()
+    
 }
