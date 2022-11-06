2f05f893787334bd40b62cd54272eb07753d067f
Darius Tanz <scratchticketseb@gmail.com>
2022-11-06 16:18:45 +0000

    Nov 6, 2022, 11:18 AM

--- Website/js/web3.js
+++ Website/js/web3.js
@@ -51,7 +51,7 @@ async function UpdateProprietorContracts(){
 
     index = 0;
     while(index < PIDs.length){
-        GetContractDetails(PIDs[index])
+        Contracts.push(GetContractDetails(PIDs[index]))
     }
 }
 
