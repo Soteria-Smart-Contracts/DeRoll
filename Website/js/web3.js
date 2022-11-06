089da632e87bfc459aadeecbaf511c183dd36cdb
Darius Tanz <scratchticketseb@gmail.com>
2022-11-06 11:31:37 +0000

    Nov 6, 2022, 6:31 AM

--- Website/js/web3.js
+++ Website/js/web3.js
@@ -27,7 +27,7 @@ async function CreateWorkContract(){
     EtherInput = BigInt(document.getElementById('EtherInput').value * 1000000000)
 
     Ether = BigInt(EtherInput * BigInt(1000000000))
-    console.log(Ether);
+    console.log(string(Ether));
     return
 
     if(Addy.length =! 42){
